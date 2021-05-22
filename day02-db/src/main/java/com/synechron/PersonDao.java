059d275de832883d6c59fa6141b404fd1b27b5a7
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-05-22 11:23:37 +0530

    Day02

--- day02-db/src/main/java/com/synechron/PersonDao.java
+++ day02-db/src/main/java/com/synechron/PersonDao.java
@@ -2,13 +2,28 @@ package com.synechron;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface PersonDao extends CrudRepository<Person, Integer> {
 	
 	//findBy and findAllBy methods
 	//methods are named on the combinations of the fields 
 	//methods vary based on the arguments 
+	
+	@Query(value = "select * from persons where age > :p1", nativeQuery = true)
+	List<Person> findAllWithAgeGt(@Param("p1") int age);
+	
+	//JPA QL : Query classes and fields
+	@Query("select p from Person p where p.age > :p1")
+	List<Person> findPersonsWithAgeGt(int age);
+	
+	@Modifying
+	@Query("update Person p set p.age = :age where p.id = :id ")
+	void updateAgeOf(@Param("id") int id, @Param("age") int newAge);
+	
 	
 	Person findByNameAndAge(String name, int age);
 	Person findByAgeAndName(int age, String name);
