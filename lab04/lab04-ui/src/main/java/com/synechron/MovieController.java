c37f105ada95af441d47460779b12ff3f399bb29
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-05-23 15:45:21 +0530

    Day03

--- lab04/lab04-ui/src/main/java/com/synechron/MovieController.java
+++ lab04/lab04-ui/src/main/java/com/synechron/MovieController.java
@@ -1,8 +1,13 @@
 package com.synechron;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MovieController {
 
+	private List<String> movieDetails = new ArrayList<>();
+	
 	@Autowired
 	private JmsTemplate jmsTemplate;
 	
@@ -21,4 +28,15 @@ public class MovieController {
 		jmsTemplate.convertAndSend(movieInputQueue, movie);
 		return "Details for " + movie + " will be fetched soon";
 	}
+	
+	@JmsListener(destination = "${movie.details.queue}")
+	public void receiveMovieDetails(String details) {
+		movieDetails.add(details);
+	}
+	
+	@GetMapping("/details")
+	public List<String> getMovieDetails() {
+		return movieDetails;
+	}
+	
 }
