1186bd092a3718ffd76094763308882bc5fea8d5
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-05-29 11:42:09 +0530

    day05

--- day05/discovery-service/src/main/java/com/synechron/DiscoveryServiceApplication.java
+++ day05/discovery-service/src/main/java/com/synechron/DiscoveryServiceApplication.java
@@ -2,14 +2,14 @@ package com.synechron;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient //Optional
-public class MathServiceApplication {
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MathServiceApplication.class, args);
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
 
 }
--- day05/ui-app/src/main/java/com/synechron/UiAppApplication.java
+++ day05/ui-app/src/main/java/com/synechron/UiAppApplication.java
@@ -2,12 +2,14 @@ package com.synechron;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @Configuration
+@EnableEurekaClient
 public class UiAppApplication {
 
 	public static void main(String[] args) {
